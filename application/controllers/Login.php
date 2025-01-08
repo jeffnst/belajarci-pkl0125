6b6868a6e1804d3ce58b0bede98a7cc110c34587
jeffnst <jeffryan_it@yahoo.co.id>
2025-01-08 16:27:06 +0700

    first commit

---
+++ application/controllers/Login.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Login extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		$this->load->view('view_login');
+	}
+}
+
+/* End of file: Login.php */
