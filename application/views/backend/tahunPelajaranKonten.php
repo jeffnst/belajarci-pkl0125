ed7e23ee9b57ecdfbd881d9d5f2b33a724e9736f
jeffnst <jeffryan_it@yahoo.co.id>
2025-01-13 16:47:45 +0700

    modal tambah tahun pelajaran

--- application/views/backend/tahunPelajaranKonten.php
+++ application/views/backend/tahunPelajaranKonten.php
@@ -5,6 +5,7 @@
 				<h3 class="card-title">Tahun Pelajaran</h3>
 			</div>
 			<div class="card-body">
+				<div class="btn btn-primary btnTambahTahunPelajaran mb-2"> <i class="fas fa-plus"></i> Tambah</div>
 				<div class="row">
 					<table class="table table-striped" id="tabelTahunPelajaran">
 						<thead>
@@ -25,6 +26,64 @@
 		</div>
 	</div>
 </div>
+
+<div class="modal" id="modalTahunPelajaran" tabindex=" -1" role="dialog">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title">Tambah Tahun Pelajaran</h5>
+
+				<button type="button" class="close " data-bs-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<div class="form-user">
+					<form action="#" method="post" enctype="multipart/form-data">
+						<input type="hidden" class="form-control" id="id" name="id" value="">
+
+						<div class="mb-1">
+							<label for="nama_tahun_pelajaran" class="form-label">Nama Tahun Pelajaran</label>
+							<input type="text" class="form-control" id="nama_tahun_pelajaran" name="nama_tahun_pelajaran" value="">
+							<div class="error-block"></div>
+						</div>
+						<div class="mb-1">
+							<label for="tanggal_mulai" class="form-label">Tanggal Mulai</label>
+							<input type="date" class="form-control" id="tanggal_mulai" name="tanggal_mulai" value="">
+							<div class="error-block"></div>
+						</div>
+						<div class="mb-1">
+							<label for="tanggal_akhir" class="form-label">Tanggal Akhir</label>
+							<input type="date" class="form-control" id="tanggal_akhir" name="tanggal_akhir" value="">
+							<div class="error-block"></div>
+						</div>
+						<div class="mb-1">
+							<label for="status_tahun_pelajaran" class="form-label">Status</label>
+							<select class="form-control" id="status_tahun_pelajaran" name="status_tahun_pelajaran">
+								<option value="1">Aktif</option>
+								<option value="0">Tidak Aktif</option>
+							</select>
+							<div class="error-block"></div>
+						</div>
+
+
+					</form>
+
+					<div>
+
+					</div>
+
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-primary saveBtn">Simpan</button>
+				<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+
 
 <script>
 	$(document).ready(function() {
@@ -61,4 +120,8 @@
 			}
 		});
 	}
+
+	$('.btnTambahTahunPelajaran').click(function() {
+		$('#modalTahunPelajaran').modal('show');
+	});
 </script>
